f695b965858f5eb95a7e328d48fc7c005a0e7fe9
gqdThinky <104022323+gqdThinky@users.noreply.github.com>
2025-06-13 00:00:16 +0200

    Improved Velocity module (bypasses anticheats)
    bro reflections do not want to work

--- src/main/java/com/amberclient/modules/combat/Velocity.java
+++ src/main/java/com/amberclient/modules/combat/Velocity.java
@@ -1,62 +1,114 @@
 package com.amberclient.modules.combat;
 
-import com.amberclient.events.EventManager;
-import com.amberclient.events.PacketReceiveListener;
-import com.amberclient.mixins.amberclient.EntityVelocityUpdateS2CPacketAccessor;
 import com.amberclient.utils.module.ConfigurableModule;
 import com.amberclient.utils.module.Module;
 import com.amberclient.utils.module.ModuleSettings;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.network.packet.Packet;
-import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Velocity extends Module implements PacketReceiveListener, ConfigurableModule {
-    private final MinecraftClient mc = MinecraftClient.getInstance();
+public class Velocity extends Module implements ConfigurableModule {
 
-    private final ModuleSettings horizontalMultiplier = new ModuleSettings("Horizontal Multiplier", "X/Z knockback multiplier", 0.0, 0.0, 1.0, 0.05);
-    private final ModuleSettings verticalMultiplier = new ModuleSettings("Vertical Multiplier", "Y knockback multiplier", 0.0, 0.0, 1.0, 0.05);
+    private final ModuleSettings horizontalScale = new ModuleSettings("Horizontal Scale", "X/Z velocity scale", 0.8, 0.0, 1.0, 0.05);
+    private final ModuleSettings verticalScale = new ModuleSettings("Vertical Scale", "Y velocity scale", 0.8, 0.0, 1.0, 0.05);
+    private final ModuleSettings contextualReduction = new ModuleSettings("Contextual", "Reduce based on game context", true);
+    private final ModuleSettings randomization = new ModuleSettings("Randomization", "Add slight randomness to avoid patterns", true);
 
     private final List<ModuleSettings> settings = new ArrayList<>();
 
-    public Velocity() {
-        super("Velocity", "Reduces or removes knockback", "Combat");
-        settings.add(horizontalMultiplier);
-        settings.add(verticalMultiplier);
-    }
+    private Vec3d lastVelocity = Vec3d.ZERO;
+    private final double[] recentReductions = new double[5];
+    private int reductionIndex = 0;
 
-    @Override
-    public void onEnable() {
-        EventManager.getInstance().add(PacketReceiveListener.class, this);
+    public Velocity() {
+        super("Velocity", "Natural knockback reduction", "Combat");
+        settings.add(horizontalScale);
+        settings.add(verticalScale);
+        settings.add(contextualReduction);
+        settings.add(randomization);
     }
 
     @Override
     public void onDisable() {
-        EventManager.getInstance().remove(PacketReceiveListener.class, this);
+        lastVelocity = Vec3d.ZERO;
+        reductionIndex = 0;
+        Arrays.fill(recentReductions, 1.0);
     }
 
     @Override
-    public void onPacketReceive(Packet<?> packet) {
-        if (!isEnabled() || !(packet instanceof EntityVelocityUpdateS2CPacket)) return;
+    public void onTick() {
+        if (!isEnabled()) return;
 
-        EntityVelocityUpdateS2CPacket velocityPacket = (EntityVelocityUpdateS2CPacket) packet;
+        PlayerEntity player = MinecraftClient.getInstance().player;
+        if (player == null) return;
 
-        if (mc.player != null && velocityPacket.getEntityId() == mc.player.getId()) {
-            double multiplierXZ = horizontalMultiplier.getDoubleValue();
-            double multiplierY = verticalMultiplier.getDoubleValue();
+        Vec3d currentVelocity = player.getVelocity();
 
-            EntityVelocityUpdateS2CPacketAccessor accessor = (EntityVelocityUpdateS2CPacketAccessor) velocityPacket;
+        if (isSignificantKnockback(currentVelocity)) {
+            Vec3d reducedVelocity = calculateReducedVelocity(player, currentVelocity);
 
-            int originalX = accessor.getVelocityX();
-            int originalY = accessor.getVelocityY();
-            int originalZ = accessor.getVelocityZ();
-
-            accessor.setVelocityX((int) (originalX * multiplierXZ));
-            accessor.setVelocityY((int) (originalY * multiplierY));
-            accessor.setVelocityZ((int) (originalZ * multiplierXZ));
+            player.setVelocity(reducedVelocity);
         }
+
+        lastVelocity = currentVelocity;
+    }
+
+    private boolean isSignificantKnockback(Vec3d velocity) {
+        PlayerEntity player = MinecraftClient.getInstance().player;
+        if (player == null) return false;
+
+        boolean isBeingDamaged = player.hurtTime > 0;
+        if (!isBeingDamaged) return false;
+
+        double horizontalThreshold = 0.15;
+        double verticalThreshold = 0.1;
+
+        boolean hasHorizontalKnockback = velocity.horizontalLength() > horizontalThreshold;
+        boolean hasVerticalKnockback = velocity.y > verticalThreshold;
+        boolean significantChange = velocity.lengthSquared() > lastVelocity.lengthSquared() * 1.3;
+
+        return (hasHorizontalKnockback || hasVerticalKnockback) && significantChange;
+    }
+
+    private Vec3d calculateReducedVelocity(PlayerEntity player, Vec3d originalVelocity) {
+        double horizontalReduction = horizontalScale.getDoubleValue();
+        double verticalReduction = verticalScale.getDoubleValue();
+
+        if (contextualReduction.getBooleanValue()) {
+            if (player.isSneaking()) {
+                horizontalReduction *= 1.2;
+            }
+
+            if (player.isOnGround()) {
+                horizontalReduction *= 1.1;
+            }
+
+            float healthFactor = player.getHealth() / player.getMaxHealth();
+            horizontalReduction *= (0.8 + healthFactor * 0.2);
+        }
+
+        if (randomization.getBooleanValue()) {
+            double randomFactor = 0.95 + (Math.random() * 0.1);
+            horizontalReduction *= randomFactor;
+            verticalReduction *= randomFactor;
+
+            recentReductions[reductionIndex] = randomFactor;
+            reductionIndex = (reductionIndex + 1) % recentReductions.length;
+        }
+
+        horizontalReduction = MathHelper.clamp(horizontalReduction, 0.0, 1.0);
+        verticalReduction = MathHelper.clamp(verticalReduction, 0.0, 1.0);
+
+        return new Vec3d(
+                originalVelocity.x * horizontalReduction,
+                originalVelocity.y * verticalReduction,
+                originalVelocity.z * horizontalReduction
+        );
     }
 
     @Override
--- src/main/java/com/amberclient/utils/module/ModuleManager.java
+++ src/main/java/com/amberclient/utils/module/ModuleManager.java
@@ -1,35 +1,46 @@
 package com.amberclient.utils.module;
 
+import com.amberclient.modules.combat.AutoClicker;
+import com.amberclient.modules.combat.Hitbox;
+import com.amberclient.modules.combat.KillAura;
+import com.amberclient.modules.combat.Velocity;
+import com.amberclient.modules.hud.ActiveMods;
+import com.amberclient.modules.hud.Transparency;
+import com.amberclient.modules.minigames.MMFinder;
+import com.amberclient.modules.movement.AutoClutch;
+import com.amberclient.modules.movement.NoFall;
+import com.amberclient.modules.movement.SafeWalk;
+import com.amberclient.modules.player.FastBreak;
+import com.amberclient.modules.player.FastPlace;
+import com.amberclient.modules.render.EntityESP;
+import com.amberclient.modules.render.Fullbright;
+import com.amberclient.modules.render.xray.Xray;
 import net.minecraft.client.MinecraftClient;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
-import org.reflections.Reflections;
-import org.reflections.scanners.Scanners;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
 
 public class ModuleManager {
     private static final ModuleManager INSTANCE = new ModuleManager();
     private final List<Module> modules = new ArrayList<>();
 
     private ModuleManager() {
-        Reflections reflections = new Reflections(new ConfigurationBuilder()
-                .setUrls(ClasspathHelper.forPackage("com.amberclient.modules"))
-                .setScanners(Scanners.SubTypes));
-
-        Set<Class<? extends Module>> moduleClasses = reflections.getSubTypesOf(Module.class);
-
-        for (Class<? extends Module> moduleClass : moduleClasses) {
-            try {
-                Module module = moduleClass.getDeclaredConstructor().newInstance();
-                registerModule(module);
-            } catch (Exception e) {
-                System.err.println("Error during module instantiation of " + moduleClass.getSimpleName() + ": " + e.getMessage());
-            }
-        }
+        registerModule(new AutoClicker());
+        registerModule(new Hitbox());
+        registerModule(new KillAura());
+        registerModule(new ActiveMods());
+        registerModule(new Transparency());
+        registerModule(new NoFall());
+        registerModule(new SafeWalk());
+        registerModule(new FastBreak());
+        registerModule(new FastPlace());
+        registerModule(new Xray());
+        registerModule(new EntityESP());
+        registerModule(new Fullbright());
+        registerModule(new MMFinder());
+        registerModule(new AutoClutch());
+        registerModule(new Velocity());
     }
 
     public static ModuleManager getInstance() { return INSTANCE; }
